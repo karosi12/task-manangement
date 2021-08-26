c1f5b408551d4ae0be0d47dcdb76fd57e598c1d0
karosi12 <adekayor@gmail.com>
2021-08-26 15:50:50 +0100

    feat: add task route

--- src/modules/shared/routes.js
+++ src/modules/shared/routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 const router = express.Router();
 import user from "../user/user.routes";
+import task from "../task/task.routes";
 
 router.get("/", (req, res) =>
   res.status(200).json({ message: "API is running" })
 );
 
 router.use("/", user);
+router.use("/task", task);
 export default router;
