c01b05ea9ed23770d1e836c69d3d39db31e1969a
karosi12 <adekayor@gmail.com>
2021-08-26 21:09:22 +0100

    feat: task crud route

--- src/modules/task/task.routes.js
+++ src/modules/task/task.routes.js
@@ -3,5 +3,8 @@ import taskCtrl from "./task.controller";
 const taskRouter = express.Router();
 
 taskRouter.post("/", taskCtrl.createTask);
+taskRouter.patch("/:id", taskCtrl.updateTask);
+taskRouter.get("/:id", taskCtrl.findTask);
+taskRouter.delete("/:id", taskCtrl.deleteTask);
 
 export default taskRouter;
